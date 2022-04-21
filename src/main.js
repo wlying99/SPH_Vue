f9dca1624fc71a27c6e81a52cc9e2057dfbd30e2
coderw <1307666094@qq.com>
2022-04-21 14:30:40 +0800

    home主页静态组件搭建

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//三级联动的组件 注册为全局组件
+import TypeNav from '@/view/Home/TypeNav'
+
+Vue.component(TypeNav.name, TypeNav)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
